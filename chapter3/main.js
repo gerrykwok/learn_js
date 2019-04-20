44b66f3c8d6ba7f9063df30b9054c1610917c931
gerrykwok <evakwok1981@hotmail.com>
2019-04-20 21:37:55 +0800

    1.加入chapter3的练习；2.把jQuery和paper库由网络下载改为本地

---
+++ chapter3/main.js
@@ -0,0 +1,114 @@
+//console.log('main.js loaded')
+
+function test01()
+{
+	var varTest;
+	let letTest;
+	console.log(varTest); //输出undefined
+	console.log(letTest); //输出undefined
+}
+
+function test02()
+{
+	console.log(varTest); //输出undefined(注意要注释掉下面一行才能运行)
+//	console.log(letTest); //直接报错：ReferenceError: letTest is not defined
+
+	var varTest = 'test var OK.';
+//	let letTest = 'test let OK.';
+}
+
+function test03()
+{
+//	Array arr;
+//	console.log("arr="+arr);
+	let a = 0.1
+	let b = 0.2
+	let c = a + b
+	console.log("a="+a)
+	console.log("b="+b)
+	console.log("a+b="+c)
+
+	let d = 022
+	console.log("d="+d)
+	let e = NaN
+	console.log("e="+e)
+	let f = Number.EPSILON;
+	console.log("f="+f)
+	let g = `abc`
+	console.log("g="+g)
+	let h = "12\u00b0C"
+	console.log("h="+h)
+}
+
+function test04()
+{
+	let curTemp = 37
+	let str = `current temp:${curTemp}\u00b0C`
+	console.log(str)
+
+	let multiline = "line1\n\
+line2"
+	console.log(`multiline=${multiline}`)
+	let multi2 = `line1
+line2`
+	console.log(`multi2=${multi2}`)
+
+	const RRR = Symbol()
+	console.log("RRR="+RRR)
+}
+
+function test05()
+{
+	const obj = {}
+//	obj.color = 1
+	console.log("obj.color="+obj.color)
+}
+
+function test06()
+{
+	const s = "hello"
+	const d = s.toUpperCase()
+	console.log(`s=${s},d=${d}`)
+	s.rating = 3
+//	s.rating;
+	console.log(`s.rating=${s.rating}`)
+}
+
+function test07()
+{
+	const a1 = [1, 2, 3]
+	a1.abc = 3
+	let len = a1.length
+//	const a2 = {abc:1,def:2}
+//	len = a2.length
+	console.log(`len=${len},a1.abc=${a1.abc}`)
+	for(let s of a1)
+	{
+		console.log(`item=${s}`)
+	}
+}
+
+function test08()
+{
+	let now = new Date(2019, 9, 1, 2, 3, 4)
+	console.log(`date=${now}`)
+	now = new Date()
+	let timestamp_ms = now.valueOf()
+	let timestamp = timestamp_ms / 1000
+	console.log(`ms=${timestamp_ms},now=${timestamp}`)
+
+	let obj = {
+		toString : function()
+		{
+			return "my obj"
+		}
+	}
+	console.log(`obj=${obj}`)
+}
+
+$(document).ready(function() {
+	'use strict';
+
+	console.log("main.js loaded")
+	test08()
+})
---
+++ lib/readme.txt
@@ -0,0 +1,2 @@
+https://code.jquery.com/jquery-2.1.1.min.js
+https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.9.24/paper-full.min.js
